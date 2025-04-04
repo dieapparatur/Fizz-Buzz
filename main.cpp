9bd0a95284feca3a6b522124752f7531ad7333f3
Fynn Negele <fynn.negele@gmx.de>
2025-04-04 10:34:58 +0200

    Funktion abgeändert

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 void FizzBuzz(int input) {
-    if (input % 3 == 0) {
-        std::cout << "Fizz";
+    if (input % 3 == 0 || input % 5 == 0) {
+        if (input % 3 == 0) {
+            std::cout << "Fizz";
+        }
         if (input % 5 == 0) {
             std::cout << "Buzz" << std::endl;
-        }
-    } else {
+        } else {
         std:: cout << input << std::endl;
     }
 

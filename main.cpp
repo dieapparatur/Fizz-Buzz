6ee8c6f6445ee08e378624083fe24c925d3f5666
Fynn Negele <fynn.negele@gmx.de>
2025-04-04 10:09:41 +0200

    Buzz implementiert

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ int main() {
     if (input % 3) {
         std::cout << "Fizz" << std::endl;
     }
+    if (input % 5) {
+        
+    }
 
     std::cout << "Hello, World!" << std::endl;
     return 0;

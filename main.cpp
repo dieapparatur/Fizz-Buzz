24cdb9343dc0bc1be2fc426ffa9e9f7d60730b50
Fynn Negele <fynn.negele@gmx.de>
2025-04-04 10:03:07 +0200

    Erster Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+
+    int input;
+
+    std::cin >> input;
+
+    if (input % 3) {
+        std::cout << "Fizz" << std::endl;
+    }
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
